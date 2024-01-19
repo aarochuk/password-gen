e7102b663f3b1e6cb200b81b969abe4f03640c3d
uglkjgj <andrewarochukwu@gmail.com>
2024-01-18 19:43:35 -0500

    done with ui and using api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import axios from "axios";
+import { passwordStrength } from 'check-password-strength';
 
 const app = express();
 
@@ -10,4 +11,22 @@ app.get('/', (req, res)=>{
     res.render("index.ejs");
 })
   
+app.post('/', async(req, res)=>{
+    var params = {
+        'upper': req.body.upper ? req.body.upper : "off",
+        'lower': req.body.lower ? req.body.lower : "off",
+        'numbers': req.body.numbers ? req.body.numbers : "off",
+        'special': req.body.special ? req.body.special : "off",
+        'length': req.body.length,
+        'repeat': 1,
+    }
+    try{
+        const response = await axios.get("https://passwordwolf.com/api/", {params : params});
+        const pass = response.data[0].password
+        const stren = passwordStrength(pass).id
+        res.render("index.ejs", {password: pass, strength: stren})
+    } catch(error){
+        console.error("Failed to make request:", error.message);
+    }
+})
 app.listen(3000)
